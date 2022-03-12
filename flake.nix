c7a7e4872b15dfb3ad992fccb07316639c965b33
hugosenari <hugosenari@gmail.com>
2022-03-12 01:49:30 -0300

    chore: re index xdo

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Nim GUI Automation Linux, simulate user interaction, mouse and keyboard.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."xdo-master".url = "path:./master";
+  inputs."xdo-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."xdo-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Nim GUI Automation Linux, simulate user interaction, mouse and keyboard.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-xdo-master.flake = false;
+  inputs.src-xdo-master.owner = "juancarlospaco";
+  inputs.src-xdo-master.ref   = "refs/heads/master";
+  inputs.src-xdo-master.repo  = "nim-xdo";
+  inputs.src-xdo-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-xdo-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-xdo-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
